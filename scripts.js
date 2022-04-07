fca9027f31bfbab90909eb541a3cc52bade5a6d1
Abin Thaha Azeez <Abin.Thaha.Azees@gds.ey.com>
2022-04-07 23:37:47 +0530

    Simple form validations added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+
+$('#form-wrapper').on('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (username.value.trim().length === 0) {
+        alert('Username cannot be empty');
+    } else if (password.value.trim().length === 0) {
+        alert('Password cannot be empty');
+    } else if (username.value === 'test@test.com' && password.value === 'password') {
+        alert('Login Success')
+    } else {
+        alert('Login credentials are incorrect, please use correct details')
+    }
+})
