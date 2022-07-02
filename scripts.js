27cdb9631223ed466311e0ae07a6eeb86a4addd5
Abin Thaha Azees <Abin.Thaha.Azees@gds.ey.com>
2022-07-02 16:59:11 +0530

    Minor changes

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ $('#submit-btn').on('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
     if (username.value.trim().length === 0) {
-        alert('Username cannot be empty');
+        // alert('Username cannot be empty');
     } else if (password.value.trim().length === 0) {
-        alert('Password cannot be empty');
+        // alert('Password cannot be empty');
     } else if (username.value === 'test@test.com' && password.value === 'password') {
-        alert('Login Success')
+        // alert('Login Success')
     } else {
-        alert('Login credentials are incorrect, please use correct details')
+        // alert('Login credentials are incorrect, please use correct details')
     }
 })
